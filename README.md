a90d3280cbb314756a707c924fb8662116cc9277
Makefile_dot_in <karlis.aritis@gmail.com>
2021-01-20 20:16:09 +0200

    Now supports more than just CV syllables

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
 # Cudigi
 A name generator for anything. Its name is generated with itself.
 # Usage
-This program is both Python 2 and 3 compatible. When launched, it will ask for minimum length, maximum length, and then it will generate the name. 
+This program is only Python 3 compatible. It takes 2 arguments: minimum number of syllables and maximum number of syllables. Additionally, the code may be edited to change the syllable type by specifying a maximum and a minimum syllable. By default, the min syllable is CV and the max syllable is CVVC
--- cudigi.py
+++ cudigi.py
@@ -1,26 +1,45 @@
-#!/usr/bin/env python2
-from __future__ import print_function
+#!/usr/bin/env python3
+import sys
 import random
-if 5/2==1.5:
-    input=lambda *args, **kwargs: raw_input(*args,**kwargs)
-consonants=['B', 'C', 'D', 'F', 'G', 'H', 'J', 'K', 'L', 'M', 'N', 'P', 
-'Q', 'R', 'S', 'T', 'V', 'X', 'Z']
-vowels=['A','E','I','O','U']
-length=random.randint(int(input("Min length: ")), int(input("Max length: ")))
-vowelflag=False
-if random.randint(0,1)==1: vowelflag=True
-wordt=[]
-for i in range(1,length):
-    if vowelflag:
-        vowelflag=False
-        wordt.append(vowels[random.randint(0,len(vowels)-1)].lower())
-    else:
-        vowelflag=True
-        wordt.append(consonants[random.randint(0,len(vowels)-1)].lower())
-capitalization=random.randint(0,1)
-if capitalization:
-    wordt[0]=wordt[0].upper()
-    final="".join(wordt)
-else:
-    final="".join(wordt).upper()
+
+consonants = list("bcdfghjklmnpqrstvwxyz") + ["ch", "sh", "th"]
+vowels = "aeiouy"
+symbols = {
+    "C": consonants,
+    "V": vowels
+}
+max_syllable = "CVVC"
+min_syllable = "CV"
+
+def string_difference(s1, s2):
+    idx = s1.find(s2)
+    if idx == -1:
+        print("Error: max syllable must include the min syllable!")
+        sys.exit(1)
+    end = idx + len(s2)
+    return s1[:idx], s1[end:]
+
+extras_before, extras_after = string_difference(max_syllable, min_syllable)    
+syllable_length = random.randint(*map(int, sys.argv[1:3]))
+word = ""
+
+def generate_from_tmplt(template):
+    return "".join(random.choice(symbols[s]) if s in symbols else s for s in template)
+
+def pick_random_phonemes(phonemes):
+    if not len(phonemes): return []
+    return random.sample(phonemes, k=random.randint(0, len(phonemes)-1))
+
+gen_random_sounds = lambda sounds: generate_from_tmplt(pick_random_phonemes(sounds))
+
+for _ in range(syllable_length):
+    minimal = generate_from_tmplt(min_syllable)
+    eb = gen_random_sounds(extras_before)
+    ea = gen_random_sounds(extras_after)
+    word += eb + minimal + ea
+
+# on a second thought, all caps names suck, removing
+wordt = list(word)
+wordt[0] = wordt[0].upper()
+final= "".join(wordt)
 print(final)
